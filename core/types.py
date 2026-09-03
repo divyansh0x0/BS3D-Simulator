0eedbcd49dacf2eb1148dc294c46845324d59b2f
divyansh0x0 <divyansh0x0@gmail.com>
2026-09-04 00:39:29 +0530

    feat: add core folder with basic types

---
+++ core/types.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass
+from typing import Literal, Callable, TypeAlias, override
+import numpy as np
+
+# 1. Custom Type Aliases
+BeamType = Literal["Rectangular", "Circular", "I-Beam"]
+LoadType = Literal["Point", "UDL", "UVL"]
+
+ArrayPair: TypeAlias = tuple[np.ndarray, np.ndarray]
+
+
+@dataclass
+class Load:
+    type: LoadType
+    magnitude: float| int
+    position: float | int
+
+# Takes (Dimensions, list[Load]) -> Returns (numpy_array, numpy_array)
+PhysicsSolver = Callable[[BeamType, list[Load]], ArrayPair]
